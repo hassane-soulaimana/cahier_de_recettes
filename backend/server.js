227ee2de329aec1deb2987a10135cd6c0bfd2ff4
hassane-soulaimana <hassane.soulaimana@laplateforme.io>
2026-05-26 14:25:33 +0200

    serveur

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connexionDB = require ('./config/db.js');
+const port = 3000;
+
+dotenv.config();
+connexionDB();
+
+const app = express();
+app.use(express.json()); // parse le json
+
+app.use('/recettes',     require('./routes/recette.js'));
+app.use('/utilisateurs', require('./routes/utilisateurs.js'));
+
+// Route de Test
+app.get('/', (req, res) => {
+  res.json({ message: '🍝 API Recettes opérationnelle !' });
+});
+
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur le port  ${port}`);
+});
