56facffd340d686a03fdb86647698e7996668fee
hassane-soulaimana <hassane.soulaimana@laplateforme.io>
2026-05-26 14:26:30 +0200

    recuper via l'id

--- backend/controllers/utilisateursController.js
+++ backend/controllers/utilisateursController.js
@@ -8,3 +8,13 @@ exports.getAll = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+exports.getOne = async (req, res) => {
+  try {
+    const utilisateur = await Utilisateur.findById(req.params.id).select('-motDePasse');
+    if (!utilisateur) return res.status(404).json({ message: 'Utilisateur non trouvé' });
+    res.json(utilisateur);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
