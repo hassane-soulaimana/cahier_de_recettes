3a1196629c5a1592ba02fa47b5c28cd45861c619
hassane-soulaimana <hassane.soulaimana@laplateforme.io>
2026-05-27 13:53:21 +0200

    rajout des routes commentaire

--- backend/routes/recette.js
+++ backend/routes/recette.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const router = express.Router();
-const CtrlR = require('../controllers/recettesController');
+const router  = express.Router();
+const ctlr   = require('../controllers/recettesController');
+const ctrlCom = require('../controllers/commentairesController');
+const { validerRecette, validerCommentaire } = require('../middleware/validation');
 
-router.get('/', CtrlR.getAll);
-router.get('/:id', CtrlR.getOne);
-router.post('/', CtrlR.create);
-router.put('/:id', CtrlR.update);
-router.delete('/:id', CtrlR.remove);
+// Recettes
+router.get('/',    ctrlr.getAll);
+router.get('/:id', ctrlr.getOne);
+router.post('/',   validerRecette, ctrlr.create);
+router.put('/:id', validerRecette, ctrlr.update);
+router.delete('/:id', ctrlr.remove);
+
+// Commentaires
+router.get('/:id/commentaires',    ctrlCom.getAll);
+router.post('/:id/commentaires',   validerCommentaire, ctrlCom.create);
+router.delete('/:id/commentaires/:commentaireId', ctrlCom.remove);
 
 module.exports = router;
