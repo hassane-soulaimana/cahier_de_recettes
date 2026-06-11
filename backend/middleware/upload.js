82688585b29572e38ad738fd6f4c10214e90e26d
hassane-soulaimana <hassane.soulaimana@laplateforme.io>
2026-06-11 14:49:37 +0200

    upload image

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -3,34 +3,32 @@ const path   = require('path');
 
 // stocker les fichiers
 const storage = multer.diskStorage({
-
-  // Dossier de destination
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+  destination: (req, file, callback) => {
+    callback(null, 'uploads/');
   },
 
   // Nom du fichier sauvegardé
-  filename: (req, file, cb) => {
+  filename: (req, file, callback) => {
     const extension = path.extname(file.originalname);
     const nomFichier = Date.now() + '-' + Math.round(Math.random() * 1e9) + extension;
-    cb(null, nomFichier);
+    callback(null, nomFichier);
   }
 });
 
 // Accepte les images 
-const fileFilter = (req, file, cb) => {
+const fileFilter = (req, file, callback) => {
   const typesAutorisés = ['image/jpeg', 'image/png', 'image/webp'];
   if (typesAutorisés.includes(file.mimetype)) {
-    cb(null, true);
+    callback(null, true);
   } else {
-    cb(new Error('Format non supporté. Utilisez JPG, PNG ou WEBP'), false);
+    callback(new Error('Format non supporté. Utilisez JPG, PNG ou WEBP'), false);
   }
 };
 
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 }
+  limits: { fileSize: 300 * 1024 * 1024 }
 });
 
 module.exports = upload;
