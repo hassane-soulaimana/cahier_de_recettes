e1d0a77845427ae0cd6d46341dc91199c77e4799
hassane-soulaimana <hassane.soulaimana@laplateforme.io>
2026-06-16 05:16:07 +0200

    ajout commentaire

--- backend/routes/recette.js
+++ backend/routes/recette.js
@@ -6,18 +6,18 @@ const { validerRecette, validerCommentaire } = require('../middleware/validation
 const upload = require ('../middleware/upload')
 const auth = require ('../middleware/auth');
 
-// Recettes
+// GET
 router.get('/',    ctrlr.getAll);
 router.get('/:id', ctrlr.getOne);
 
-
-router.post('/',   upload.single('image'), ctrlr.create);
-router.put('/:id', upload.single('image'), ctrlr.update);
-router.delete('/:id', ctrlr.remove);
+//  POST, PUT, DELETE
+router.post('/',   auth, upload.single('image'), ctrlr.create);
+router.put('/:id', auth, upload.single('image'), ctrlr.update);
+router.delete('/:id', auth, ctrlr.remove);
 
 // Commentaires
 router.get('/:id/commentaires',    ctrlCom.getAll);
-router.post('/:id/commentaires',   validerCommentaire, ctrlCom.create);
-router.delete('/:id/commentaires/:commentaireId', ctrlCom.remove);
+router.post('/:id/commentaires',   auth, validerCommentaire, ctrlCom.create);
+router.delete('/:id/commentaires/:commentaireId', auth, ctrlCom.remove);
 
 module.exports = router;
